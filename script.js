715cc37c14cc838d5fbbe6602aa55952020fe75d
mojvaf <mvafa44@gmail.com>
2020-10-31 23:36:04 +0100

    added javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const days = document.getElementById('days')
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+const countdown = document.getElementById('countdown')
+
